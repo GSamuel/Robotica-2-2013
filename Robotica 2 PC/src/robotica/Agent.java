c6122fbe5ba9a7452217a06922d0c06391d8f176
GSamuel <gideon_hoog@hotmail.com>
2013-10-04 12:42:18 +0200

    Signed-off-by: GSamuel <gideon_hoog@hotmail.com>

--- Robotica 2 PC/src/robotica/Agent.java
+++ Robotica 2 PC/src/robotica/Agent.java
@@ -1,28 +1,15 @@
 package robotica;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
 public abstract class Agent
 {
 	private int localID;
 	private ArrayList<State> states;
-	private HashMap<String,Object> map;
 
 	public Agent()
 	{
 		states = new ArrayList<State>();
-		map = new HashMap<String,Object>();
-	}
-	
-	public void setVar(String key, int value)
-	{
-		map.put(key, value);
-	}
-	
-	public Object getValue(String key)
-	{
-		return map.get(key);
 	}
 
 	public void addState(State state)
--- Robotica 2 PC/src/robotica/NoAgent.java
+++ Robotica 2 PC/src/robotica/NoAgent.java
@@ -5,6 +5,5 @@ public class NoAgent extends Agent
 	public NoAgent()
 	{
 		addState(new NoState(this));
-		setVar("isDoingSomething", 0);
 	}
 }
