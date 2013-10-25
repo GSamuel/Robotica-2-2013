10136fff4ff27983a97c05418379609e38663d3e
GSamuel <gideon_hoog@hotmail.com>
2013-10-25 14:27:45 +0200

    test

--- Robotica 2 PC/src/robotica/SimAgent.java
+++ Robotica 2 PC/src/robotica/SimAgent.java
@@ -4,6 +4,13 @@ public class SimAgent extends Agent
 {
 	public SimAgent()
 	{
+		super("SimName", new NoState(0));
+	}
+
+	@Override
+	public void update()
+	{
+		// TODO Auto-generated method stub
 		
 	}
 }
