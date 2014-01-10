f2e1940c2e776bcae6a6be47a0ba2e996b8c68df
Guy92 <guy.ackermans92@gmail.com>
2014-01-10 16:57:33 +0100

    Customerbehavior

--- Robotica 2 NXT/src/agent/Customer.java
+++ Robotica 2 NXT/src/agent/Customer.java
@@ -14,21 +14,22 @@ public class Customer extends Agent {
 	private long stopwatch;
 	private CustomerBehavior behavior;
 	private int wachttijd;
-	
-	//Motor.A vervangen door motor, zodat een customer aan een specifieke motor gekoppeld kan worden.   Yay dynamischheid
+
+	// Motor.A vervangen door motor, zodat een customer aan een specifieke motor
+	// gekoppeld kan worden. Yay dynamischheid
 	private NXTRegulatedMotor motor;
 
 	public Customer(NXTRegulatedMotor motor, String name) {
 		super(name, new SimState("IDLE"));
-		this.wachttijd = 3;
+		this.wachttijd = 10;
 		this.behavior = new CustomerBehavior(wachttijd);
 		init = true;
 		starttime = System.currentTimeMillis();
 		currenttime = System.currentTimeMillis();
 		stopwatch = 0;
-		//this.setState(new SimState("NEUTRAAL"));
-		//setChanged();
-		
+		// this.setState(new SimState("NEUTRAAL"));
+		// setChanged();
+
 		this.motor = motor;
 		motor.resetTachoCount();
 	}
@@ -37,7 +38,7 @@ public class Customer extends Agent {
 	public void update() {
 		currenttime = System.currentTimeMillis();
 		stopwatch = currenttime - starttime;
-		//System.out.println(stopwatch);
+		// System.out.println(stopwatch);
 		switch (currentState().name()) {
 		case "IDLE":
 			idle();
@@ -59,64 +60,45 @@ public class Customer extends Agent {
 	}
 
 	private void idle() {
-			motor.setSpeed(720);
-			motor.rotateTo(0);
-			behavior.idle();
-			this.setState(new SimState("WBESTELLEN"));
-			setChanged();
+		System.out.println("IDLE");
+		motor.setSpeed(720);
+		motor.rotateTo(0);
+		behavior.idle();
+		this.setState(new SimState("WBESTELLEN"));
+		setChanged();
 	}
 
 	private void wBestellen() {
-			motor.setSpeed(720);
-			motor.rotateTo(90);
+		System.out.println("WBESTELLEN");
+		motor.setSpeed(720);
+		motor.rotateTo(90);
 	}
 
 	private void wEten() {
-			motor.setSpeed(720);
-			motor.rotateTo(180);
-			Random rand= new Random();
-			try {
-				Thread.sleep(rand.nextInt(2000));
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+		motor.setSpeed(720);
+		motor.rotateTo(180);
 	}
 
 	private void eten() {
-			motor.setSpeed(300);
-			motor.forward();
-			Random rand= new Random();
-			try {
-				Thread.sleep(rand.nextInt(5000));
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+		motor.setSpeed(300);
+		motor.forward();
+		wBetalen();
 	}
 
 	private void wBetalen() {
-			motor.setSpeed(720);
-			int lol= motor.getTachoCount()/360;
-			motor.rotateTo(lol*360);
-			motor.resetTachoCount();
-			motor.setSpeed(720);
-			motor.rotateTo(270);
-			Random rand= new Random();
-			try {
-				Thread.sleep(rand.nextInt(2000));
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+		motor.setSpeed(720);
+		int lol = motor.getTachoCount() / 360;
+		motor.rotateTo(lol * 360);
+		motor.resetTachoCount();
+		motor.setSpeed(720);
+		motor.rotateTo(270);
 	}
-
 }
 
 /*
  * 5 states:
  * 
- * -idle"IDLE" Niks boven. -Wachten op bestellen: "WBESTELLEN" Rood
- * boven. -Wachten op eten: "WETEN" Blauw boven. -Eten: "ETEN" Motor draait
- * rond. -Betalen: "WBETALEN" Zwart boven.
+ * -idle"IDLE" Niks boven. -Wachten op bestellen: "WBESTELLEN" Rood boven.
+ * -Wachten op eten: "WETEN" Blauw boven. -Eten: "ETEN" Motor draait rond.
+ * -Betalen: "WBETALEN" Zwart boven.
  */
